0973f57f1318445f89e23014f0c41b1f9f5733ff
jyoti <jyotidhage10@gmail.com>
2022-08-08 17:44:00 +0530

    [Aaradhya]Add:completed Basic core program Assignment

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace BasicCoreProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FlipCoin.CalculatePercentage();
+            LeapYear.FindLeapYear();
+            PowerOfTwo.PowerOfTwoSolution();
+            HarmonicNumbers.HarmonicNumberSolution();
+            PrimeFactor.PrimeFactorSolution();
+            QuotientRemaindercs.CalculateQuotientRemainder();
+            SwapTwoNumbers.NumberSwaping();
+            EvenOdd.FindEvenOdd();
+            VowelConsonant.VowelOrConsonant();
+            FindLargest.FindLargestNumber();
+
+        }
+
+
+
+
+            }
+    }
