06c6cd9ca74bac7d5717095dec79b0fd6b8f2058
jyoti <jyotidhage10@gmail.com>
2022-08-08 18:50:12 +0530

    [Aaradhya]Add:Even odd Numbers solution

---
+++ EvenOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public static class EvenOdd
+    {
+        public static void FindEvenOdd()
+        {
+            int Number = 4;
+            if (Number % 2 == 0) ;
+            {
+                Console.WriteLine("Number is Even");
+
+                 }
+            else
+                {
+                    Console.WriteLine("Number is odd");
+                }
+            }
+        }
+    }
