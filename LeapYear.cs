b861b6d55fe095d4fbbf55e6010bdbbffa613f15
jyoti <jyotidhage10@gmail.com>
2022-08-08 18:14:24 +0530

    [Aaradhya]Add:completed leap year problem

---
+++ LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public static class LeapYear
+    {
+        public static void FindLeapYear()
+        {
+            int year;
+           
+            Console.Write("Check whether a given year is leap year or not:");
+          
+           
+            Console.Write("Input an Year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 400) == 0)
+                Console.WriteLine("Year is leap Year",year);
+            else if ((year % 100) == 0)
+                Console.WriteLine("Year is not a leap Year",year );
+            else if ((year % 4) == 0)
+                Console.WriteLine("Year is a leap Year" ,year);
+            else
+                Console.WriteLine("Year is not a leap Year",year);
+        }
+    }
+}
+    
+
