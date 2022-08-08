cfb2af2fdbcaca99530263127482887cce7eff80
jyoti <jyotidhage10@gmail.com>
2022-08-08 18:42:10 +0530

    [Aaradhya]Add:Swap two Numbers solution

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+   public static  class SwapTwoNumbers
+    {
+        public static void NumberSwaping()
+        {
+            int a = 10;
+            int b = 20;
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("After swap the value is" +a +b);
+
+        }
+    }
+}
