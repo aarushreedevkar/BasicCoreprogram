e201b9949c1c8bcc6fcf8d57aa9a4f572de0c96f
jyoti <jyotidhage10@gmail.com>
2022-08-08 18:30:38 +0530

    [Aaradhya]Add:Quotient Remainder solution

---
+++ QuotientRemaindercs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public static class QuotientRemaindercs
+    {
+        public static void CalculateQuotientRemainder()
+        {
+            int dividend = 50;
+            int divisor = 8;
+            int remainder = dividend / divisor;
+            int quotient = dividend * divisor;
+            Console.WriteLine("Dividend:" +dividend+ "Divisor:" +divisor);
+            Console.WriteLine("Quotient:" +quotient);
+            Console.WriteLine("Remainder:" +remainder);
+        } 
+    }
+}
